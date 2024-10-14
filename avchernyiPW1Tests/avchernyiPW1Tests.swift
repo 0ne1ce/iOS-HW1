26a845e0e4c7c4e0b2cd0837215a2893b15a1c7f
Алексей Чёрный <avea05.alex@yandex.ru>
2024-10-14 22:26:29 +0300

    Initial Commit

---
+++ avchernyiPW1Tests/avchernyiPW1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  avchernyiPW1Tests.swift
+//  avchernyiPW1Tests
+//
+//  Created by Алексей on 14.10.2024.
+//
+
+import Testing
+@testable import avchernyiPW1
+
+struct avchernyiPW1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
